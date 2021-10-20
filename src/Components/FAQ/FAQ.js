0653539213f5ee9522476093b7780d1863662ffe
Naz365 <nazmul.gub.cse@gmail.com>
2021-10-20 23:38:12 +0600

    sotti atai final commit

--- src/Components/FAQ/FAQ.js
+++ src/Components/FAQ/FAQ.js
@@ -4,105 +4,111 @@ import faqImg from "../img/FAQs-amico.png";
 const FAQ = () => {
   return (
     <div className="container">
-        <br /><br /><br /><br /><br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <div className="row">
-      <div className="col-md-6">
-        <div className="accordion" id="accordionPanelsStayOpenExample">
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="panelsStayOpen-headingOne">
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#panelsStayOpen-collapseOne"
-                aria-expanded="true"
-                aria-controls="panelsStayOpen-collapseOne"
+        <div className="col-md-6">
+          <div className="accordion" id="accordionPanelsStayOpenExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+                <button
+                  className="accordion-button"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseOne"
+                  aria-expanded="true"
+                  aria-controls="panelsStayOpen-collapseOne"
+                >
+                  FAQ no #1
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseOne"
+                className="accordion-collapse collapse show"
+                aria-labelledby="panelsStayOpen-headingOne"
               >
-                FAQ no #1
-              </button>
-            </h2>
-            <div
-              id="panelsStayOpen-collapseOne"
-              className="accordion-collapse collapse show"
-              aria-labelledby="panelsStayOpen-headingOne"
-            >
-              <div className="accordion-body">
-                
-                <strong>What is Home Health Care?</strong> Home Health Care encompasses a wide range of health care services provided in the patient’s home with the purpose of maintaining his or her maximal level of function, health, and comfort. Home care is a collaborative effort involving family, physician, and an interdisciplinary home care team. It is a cost-effective alternative to extended hospitalization, rehabilitation, or a nursing home stay. Patients are usually more comfortable in their own home and studies have shown patients recover quicker at home.
-
-{" "}
-                 though the transition does limit
-                overflow.
+                <div className="accordion-body">
+                  <strong>What is Home Health Care?</strong> Home Health Care
+                  encompasses a wide range of health care services provided in
+                  the patient’s home with the purpose of maintaining his or her
+                  maximal level of function, health, and comfort. Home care is a
+                  collaborative effort involving family, physician, and an
+                  interdisciplinary home care team. It is a cost-effective
+                  alternative to extended hospitalization, rehabilitation, or a
+                  nursing home stay. Patients are usually more comfortable in
+                  their own home and studies have shown patients recover quicker
+                  at home. though the transition does limit overflow.
+                </div>
               </div>
             </div>
-          </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
-              <button
-                className="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#panelsStayOpen-collapseTwo"
-                aria-expanded="false"
-                aria-controls="panelsStayOpen-collapseTwo"
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseTwo"
+                >
+                  FAQ no #2
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseTwo"
+                className="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingTwo"
               >
-                FAQ no #2
-              </button>
-            </h2>
-            <div
-              id="panelsStayOpen-collapseTwo"
-              className="accordion-collapse collapse"
-              aria-labelledby="panelsStayOpen-headingTwo"
-            >
-              <div className="accordion-body">
-                <strong>This is the second item's accordion body.</strong> It is
-                hidden by default, until the collapse plugin adds the
-                appropriate classes that we use to style each element. These
-                classes control the overall appearance, as well as the showing
-                and hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
+                <div className="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the though
+                  the transition does limit overflow.
+                </div>
               </div>
             </div>
-          </div>
-          <div className="accordion-item">
-            <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-              <button
-                className="accordion-button collapsed"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#panelsStayOpen-collapseThree"
-                aria-expanded="false"
-                aria-controls="panelsStayOpen-collapseThree"
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="panelsStayOpen-headingThree">
+                <button
+                  className="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#panelsStayOpen-collapseThree"
+                  aria-expanded="false"
+                  aria-controls="panelsStayOpen-collapseThree"
+                >
+                  FAQ no #3
+                </button>
+              </h2>
+              <div
+                id="panelsStayOpen-collapseThree"
+                className="accordion-collapse collapse"
+                aria-labelledby="panelsStayOpen-headingThree"
               >
-              FAQ no #3
-              </button>
-            </h2>
-            <div
-              id="panelsStayOpen-collapseThree"
-              className="accordion-collapse collapse"
-              aria-labelledby="panelsStayOpen-headingThree"
-            >
-              <div className="accordion-body">
-                <strong>This is the third item's accordion body.</strong> It is
-                hidden by default, until the collapse plugin adds the
-                appropriate classes that we use to style each element. These
-                classes control the overall appearance, as well as the showing
-                and hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
+                <div className="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It
+                  is hidden by default, until the collapse plugin adds the
+                  appropriate classes that we use to style each element. These
+                  classes control the overall appearance, as well as the showing
+                  and hiding via CSS transitions. You can modify any of this
+                  with custom CSS or overriding our default variables. It's also
+                  worth noting that just about any HTML can go within the does
+                  limit overflow.
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="col-md-6">
-        <img className="img-fluid" src={faqImg} alt="" srcset="" />
-      </div>
+        <div className="col-md-6">
+          <img className="img-fluid" src={faqImg} alt="" srcset="" />
+        </div>
       </div>
     </div>
   );
